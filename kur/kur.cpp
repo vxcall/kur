d6a14f60fd235a8277d9a027233ecfb5dbc7332d
pseuxide <s3pt3mb3r@protonmail.com>
2023-11-10 22:24:41 +0900

    made it from app to lib

--- kur/kur.cpp
+++ kur/kur.cpp
@@ -11,9 +11,9 @@ auto kur_t::read(void* address, void* buffer, size_t buffer_size, HANDLE h_targe
   return vul_driver.ioctl_mm_copy_virtual_memory(address, buffer, buffer_size, h_target_process);
 }
 
-auto kur_t::write(void* address, void* buffer, size_t buffer_size, HANDLE h_target_process) -> BOOL
+auto kur_t::write(void* to_address, void* from_address, size_t size, HANDLE h_target_process) -> BOOL
 {
-  return vul_driver.ioctl_mm_copy_virtual_memory(address, buffer, buffer_size, h_target_process);
+  return vul_driver.ioctl_mm_copy_virtual_memory(from_address, to_address, size, h_target_process);
 }
 
 auto kur_t::query_device_handle() const -> HANDLE
